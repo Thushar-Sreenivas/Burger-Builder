9d8eb97aaa632a1c656990232e3e2f2eb1bba1db
Thushar Sreenivas <Thushar-Sreenivas@users.noreply.github.com>
2020-04-10 19:59:13 +0530

    Adding burger ingredients with initail value

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -3,13 +3,19 @@ import classes from './Burger.css'
 import BurgerIngredients from './BurgerIngredients/BurgerIngredient'
 const burger = (props) => {
 
-    const transformedIngredients = Object.keys(props.ingredients)
+    let transformedIngredients = Object.keys(props.ingredients)
     .map(igKey => { 
         return[...Array(props.ingredients[igKey])].map((_, i) => {
                     return <BurgerIngredients type={igKey} key={igKey + i}/>
             })
         }
-    )
+    ).reduce((totArr,currValue) => {
+        return totArr.concat(currValue)
+    },[])
+    if (transformedIngredients.length === 0) {
+        transformedIngredients = <p>Please add ingredients!</p>
+    }
+    console.log(transformedIngredients)
     return(
         <div className={classes.Burger}>
             <BurgerIngredients type="bread-top" />
